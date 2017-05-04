b964b6f26a58521b5a8e2295d671008e73110ca6
OLIMPIA\dam115 <tamaramartinezsa@gmail.com>
2017-05-04 10:15:58 +0200

    Clase xeometría

---
+++ src/xeometria/Xeometria.java
@@ -0,0 +1,70 @@
+package xeometria;
+
+public class Xeometria {
+
+	/**
+	 * Método que calcula o perímetro dun cadrado
+	 * @param lado Lonxitude do lado
+	 * @return Devolve o perímetro
+	 */
+	
+	public int perimetroCadrado(int lado) {
+		int perimetro;
+		perimetro = lado * 4;
+
+		if (lado < 0)
+			return -1;
+		else
+			return perimetro;
+	}
+
+	/**
+	 * Método que calcula a área dun cadrado
+	 * @param lado Lonxitude do lado
+	 * @return Devolve a área
+	 */
+
+	public int areaCadrado(int lado) {
+		int area;
+		area = lado * lado;
+
+		if (lado < 0)
+			return -1;
+		else
+			return area;
+	}
+
+	/**
+	 * Método que calcula a hipotenusa utilizando o teorema de Pitágoras
+	 * @param catetoA Lonxitude do cateto A
+	 * @param catetoB Lonxitude do cateto B
+	 * @return Devolve a hipotenusa
+	 */
+
+	public double teoremaPitagoras(int catetoA, int catetoB) {
+		double hipotenusa;
+		hipotenusa = Math.sqrt((catetoA * catetoA + catetoB * catetoB));
+
+		if (catetoA < 0 || catetoB < 0)
+			return -1;
+		else
+			return hipotenusa;
+	}
+	
+	/**
+	 * Método que calcula un cateto utilizando o teorema de Pitágoras
+	 * @param hipotenusa Lonxitude da hipotenusa
+	 * @param catetoA Lonxitude do cateto A
+	 * @return Devolve o cateto B
+	 */
+	
+	public double teoremaPitagoras(double hipotenusa, double catetoA){
+		double catetoB;
+		catetoB = Math.sqrt((hipotenusa * hipotenusa - catetoA * catetoA));
+		
+		if (hipotenusa < 0 || catetoA < 0)
+			return -1;
+		else
+			return catetoB;
+	}
+}
