10a33d1a42a4a0790d4188e3c326464cb6ac98ba
OLIMPIA\dam115 <tamaramartinezsa@gmail.com>
2017-05-18 10:16:12 +0200

    Aclaracións das comprobacións.

--- src/xeometria/Xeometria.java
+++ src/xeometria/Xeometria.java
@@ -2,36 +2,37 @@ package xeometria;
 /**
  * 
  * @author dam115
- * Se os métodos devolven o valor -1 significa que houbo un erro
+ * Se os métodos devolven o valor -1 significa que houbo un erro.
  */
 public class Xeometria {
 
 	/**
-	 * Método que calcula o perímetro dun cadrado
-	 * @param lado Lonxitude do lado
-	 * @return Devolve o perímetro
+	 * Método que calcula o perímetro dun cadrado.
+	 * @param lado Lonxitude do lado.
+	 * @return Devolve o perímetro.
 	 */
 	
 	public int perimetroCadrado(int lado) {
-		//Compróbase que o lado sexa positivo
+		//Compróbase que o lado sexa positivo e que o seu valor non sexa cero.
 		
 		if (lado <= 0)
 			return -1;
 
-		//Defínese a variable perímetro e calcúlase o perímetro
+		//Defínese a variable perímetro e calcúlase o perímetro.
 		int perimetro;
 		perimetro = lado * 4;
+		
 		return perimetro;
 	}
 
 	/**
-	 * Método que calcula a área dun cadrado
-	 * @param lado Lonxitude do lado
-	 * @return Devolve a área
+	 * Método que calcula a área dun cadrado.
+	 * @param lado Lonxitude do lado.
+	 * @return Devolve a área.
 	 */
 
 	public int areaCadrado(int lado) {
-		//Compróbase que o lado sexa positivo
+		//Compróbase que o lado sexa positivo e que o seu valor non sexa cero.
 		
 		if (lado <= 0)
 			return -1;
@@ -39,6 +40,7 @@ public class Xeometria {
 		//Defínese a variable area e calcúlase a área
 		int area;
 		area = lado * lado;
+		
 		return area;
 	}
 
@@ -50,7 +52,7 @@ public class Xeometria {
 	 */
 
 	public double teoremaPitagoras(int catetoA, int catetoB) {
-		//Compróbase que os catetos sexan positivos
+		//Compróbase que os catetos sexan positivos e que o valor destes non sexa cero.
 		
 		if (catetoA <= 0 || catetoB <= 0)
 			return -1;
@@ -59,28 +61,30 @@ public class Xeometria {
 		
 		double hipotenusa;
 		hipotenusa = Math.sqrt((catetoA * catetoA + catetoB * catetoB));
+		
 		return hipotenusa;
 	}
 	
 	/**
-	 * Método que calcula un cateto utilizando o teorema de Pitágoras
-	 * @param hipotenusa Lonxitude da hipotenusa
-	 * @param catetoA Lonxitude do cateto A
-	 * @return Devolve o cateto B
+	 * Método que calcula un cateto utilizando o teorema de Pitágoras.
+	 * @param hipotenusa Lonxitude da hipotenusa.
+	 * @param catetoA Lonxitude do cateto A.
+	 * @return Devolve o cateto B.
 	 */
 	
 	public double teoremaPitagoras(double hipotenusa, double catetoA){
 
-		// Compróbase que a hipotenusa sexa maior que o cateto
-		// Compróbase que a hipotenusa e o catetoA sexan positivos
+		/* Compróbase que a hipotenusa sexa maior que o cateto, que a hipotenusa e o catetoA sexan positivos
+		 * e que ningún deles vala cero*/
 		
-		if (hipotenusa < catetoA||hipotenusa <= 0 || catetoA <= 0)
+		if (hipotenusa < catetoA || hipotenusa <= 0 || catetoA <= 0)
 			return -1;
 		
 		// Definición da variable catetoB e cálculo do catetoB
 		
 		double catetoB;
 		catetoB = Math.sqrt((hipotenusa * hipotenusa - catetoA * catetoA));
+		
 		return catetoB;
 	}
 }
--- src/xeometria/XeometriaTest.java
+++ src/xeometria/XeometriaTest.java
@@ -29,41 +29,92 @@ public class XeometriaTest {
 	@Test
 	public void testPerimetroCadrado() {
 		Xeometria xeometria = new Xeometria();
+		
+		//Introdúcese o lado, que mide 2. O perímetro debe ser 8.
 		assertEquals(8, xeometria.perimetroCadrado(2));
+		
+		//Introdúcese un lado negativo.
 		assertEquals(-1, xeometria.perimetroCadrado(-2));
+		
+		//Introdúcese un lado con valor cero.
 		assertEquals(-1, xeometria.perimetroCadrado(0));
 	}
 
 	@Test
 	public void testAreaCadrado() {
 		Xeometria xeometria = new Xeometria();
+		
+		//Introdúcese o lado que mide 2. A área debe ser 4.
 		assertEquals(4, xeometria.areaCadrado(2));
+		
+		//Introdúcese un lado negativo.
 		assertEquals(-1, xeometria.areaCadrado(-2));
+		
+		//Introdúcese un lado con valor cero.
 		assertEquals(-1, xeometria.areaCadrado(0));
 	}
 
+	/**
+	 * O terceiro argumento do assertEquals indica a precisión dos decimais. Neste
+	 * caso é cero para comprobar que o resultado da proba coindida co primeiro 
+	 * argumento de forma exacta.
+	 */
+	
 	@Test
+	
 	public void testTeoremaPitagorasIntInt() {
 		Xeometria xeometria = new Xeometria();
+		
+		/*Introdúcense os catetos A e B, que miden 3 e 4 respectivamente.
+		A hipotenusa debe valer 5.*/
 		assertEquals(5.0, xeometria.teoremaPitagoras(3, 4), 0);
+		
+		//Introdúcese o cateto A con valor negativo.
 		assertEquals(-1, xeometria.teoremaPitagoras(-3, 4), 0);
+		
+		//Introdúcese o cateto B con valor negativo.
 		assertEquals(-1, xeometria.teoremaPitagoras(3, -4), 0);
+		
+		//Introdúcense dous catetos negativos.
 		assertEquals(-1, xeometria.teoremaPitagoras(-3, -4), 0);
+		
+		//Introdúcese o cateto A con valor cero.
 		assertEquals(-1, xeometria.teoremaPitagoras(0, 4), 0);
+		
+		//Introdúcese o cateto B con valir cero.
 		assertEquals(-1, xeometria.teoremaPitagoras(3, 0), 0);
+		
+		//Introdúcense dous catetos con valor cero.
 		assertEquals(-1, xeometria.teoremaPitagoras(0, 0), 0);
 	}
 
 	@Test
 	public void testTeoremaPitagorasDoubleDouble() {
 		Xeometria xeometria = new Xeometria();
+		
+		/*Introdúcese a hipotenusa, que vale 10 e o cateto A que vale
+		 * 8. O cateto B debe medir 6*/
 		assertEquals(6.0, xeometria.teoremaPitagoras(10.0, 8.0), 0);
-		assertEquals(-1, xeometria.teoremaPitagoras(-10.0, 8.0), 0);
-		assertEquals(-1, xeometria.teoremaPitagoras(10.0, -8.0), 0);
-		assertEquals(-1, xeometria.teoremaPitagoras(-10.0, -8.0), 0);
-		assertEquals(-1, xeometria.teoremaPitagoras(10.0, 0.0), 0);
-		assertEquals(-1, xeometria.teoremaPitagoras(0.0, 8.0), 0);
-		assertEquals(-1, xeometria.teoremaPitagoras(0.0, 0.0), 0);
+		
+		//O cateto A é maior que a hipotenusa.
 		assertEquals(-1, xeometria.teoremaPitagoras(10.0, 20.0), 0);
+		
+		//Introdúcese a hipotenusa con valor negativo.
+		assertEquals(-1, xeometria.teoremaPitagoras(-10.0, 8.0), 0);
+		
+		//Introdúcese o cateto A con valor negativo.
+		assertEquals(-1, xeometria.teoremaPitagoras(10.0, -8.0), 0);
+		
+		//Introdúcense a hipotenusa e o cateto A, os dous con valor negativo.
+		assertEquals(-1, xeometria.teoremaPitagoras(-10.0, -8.0), 0);
+		
+		//Introdúcese a hipotenusa con valor cero.
+		assertEquals(-1, xeometria.teoremaPitagoras(0.0, 8.0), 0);
+		
+		//Introdúcese o cateto A con valor cero.
+		assertEquals(-1, xeometria.teoremaPitagoras(10.0, 0.0), 0);
+		
+		//Introdúcense a hipotenusa e o cateto, os dous con valor cero.
+		assertEquals(-1, xeometria.teoremaPitagoras(0.0, 0.0), 0);
 	}
 }
